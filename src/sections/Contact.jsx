f2d0c0ed72acd4afe0972817e0d5e53e7c8b3b9e
m11toh <martohmashah@gmail.com>
2024-09-26 09:45:48 +0300

    started with contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useDarkMode } from '../components/DarkModeContext'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
+
 
 const Contact = () => {
+
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+
+    }); // initialise with default settings
+
+  }, [])
+
+  const { darkMode, toggleDarkmode } = useDarkMode();
   return (
-    <div>Contact</div>
+    <div className={`${darkMode ? 'dark:bg-black' : 'light bg-transparent'} pb-20`}>
+      <section id='contact' className={`${darkMode ? 'dark bg-gray-800' : 'light bg-red-100'} lg:w-[95%] w-full h-fit rounded-xl grid lg:grid-cols-2 grid-cols-1 justify-center items-center lg:px-36 py-20 gap-10`}>
+        
+      </section>
+
+    </div>
   )
 }
 
